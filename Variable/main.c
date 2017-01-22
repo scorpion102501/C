f143d2de9cfb6c043675b103bcad795d800d1414
Scor <scorpion102501@gmail.com>
2017-01-23 00:43:28 +0800

    add variable

---
+++ Variable/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	//じ 
+	char c = 'A';
+	//俱计 
+	int i = 23;
+	//俱计 
+	long l = 256666666;
+	//疊翴计Τ7计 
+	float f = 1.1111111;
+	//疊翴计Τ15计
+	double d = 1.111111111111111;
+	
+	//俱计
+	int a=1, b=2;
+	
+	 //块ㄢ计
+	 int x,y;
+	 printf("X => ");
+	 scanf("%d", &x);
+	 printf("Y => ");
+	 scanf("%d", &y);
+	 printf("X + Y = %d\n", x + y); 
+	return 0;
+}
